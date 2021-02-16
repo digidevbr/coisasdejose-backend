33c68f6951f43ac02a42fadc6f918adc2d2fb51a
Tiago Carpanese <tiago.carpanese@db1.com.br>
2021-02-16 20:24:08 -0300

    First Commit #1

---
+++ src/main/java/br/com/coisasdejose/coisasdejosebackend/CoisasdejoseBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.coisasdejose.coisasdejosebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoisasdejoseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoisasdejoseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coisasdejose/coisasdejosebackend/CoisasdejoseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coisasdejose.coisasdejosebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoisasdejoseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
