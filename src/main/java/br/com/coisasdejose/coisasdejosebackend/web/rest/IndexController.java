f772162c40f66030a721355b471deea5d4faf2fd
Tiago Carpanese <tiago.carpanese@db1.com.br>
2021-02-26 23:27:25 -0300

    Aula 14 #1

--- src/main/java/br/com/coisasdejose/coisasdejosebackend/web/rest/IndexController.java
+++ src/main/java/br/com/coisasdejose/coisasdejosebackend/web/rest/IndexController.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/")
 public class IndexController {
@@ -17,6 +19,6 @@ public class IndexController {
     @GetMapping
     public ResponseEntity<String> getIndex() {
         log.info("Teste de log debug");
-        return new ResponseEntity<>("Coisas de José!", HttpStatus.OK);
+        return new ResponseEntity<>("Plataforma de Receitas Coisas de José!", HttpStatus.OK);
     }
 }
